9dc808f1ffb4463e29d740e0e3f81bd9b13fd405
Manne Moquist <mannemoquist@gmail.com>
2020-05-06 17:08:27 +0200

    Added some basic libc things
    	new file:   include/stdio.h
    	new file:   include/string.h

---
+++ include/stdio.h
@@ -0,0 +1,11 @@
+#pragma once
+/* defines */
+#define EOF (-1)
+#define NULL ((void*)0)
+
+/* Formatted input output */
+int printf(const char * format, ...);
+
+/* Character input output */
+int putchar(int character);
+int puts(const char * str);
